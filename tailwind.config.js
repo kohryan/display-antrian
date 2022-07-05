7c788f7fa6b81c5babc93624b36dbfa2ac7444e4
Ryan W. Januardi <100107440+kohryan@users.noreply.github.com>
2022-07-05 20:35:55 +0800

    add config

---
+++ tailwind.config.js
@@ -0,0 +1,186 @@
+const plugin = require("tailwindcss/plugin");
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {  
+  content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    options: {
+      safelist: [],
+    },
+  theme: {
+    colors: {
+      'white': '#fff',
+      'red': '#dc2626',
+      'blue': '#2563eb',
+      'pst': '#1e40af',
+      'indigo': {
+        50: '#eef2ff',
+        100: '#e0e7ff',
+        200: '#c7d2fe',
+        300: '#a5b4fc',
+        400: '#818cf8',
+        500: '#6366f1',
+        600: '#4f46e5',
+        700: '#4338ca',
+        800: '#3730a3',
+        900: '#312e81'
+        },
+      'pink': '#ff49db',
+      'orange': '#f97316',
+      'green': '#84cc16',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': {
+        50: '#f9fafb',
+        100: '#f3f4f6',
+        200: '#e5e7eb',
+        300: '#d1d5db',
+        400: '#9ca3af',
+        500: '#6b7280',
+        600: '#4b5563',
+        700: '#374151',
+        800: '#1f2937',
+        900: '#111827'
+        },
+      'gray-light': '#d3dce6',
+      'gradient1': '#e2ffe5',
+      'gradient2': '#e5f6ff',
+      'gradient3': '#e6e2ff',
+      'smart': '#181a5a',
+      'smartlight': '#222468',
+      'smartred': '#ff0e00'
+    },
+
+
+    extend: {      
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        serif: ['Merriweather', 'serif'],
+      },
+
+      outlineWidth: {
+       0: '0px',
+       1: '1px',
+       2: '2px',
+      },
+
+      minHeight: {
+        "screen-75": "75vh",
+      },
+      fontSize: {
+        55: "55rem",
+      },
+      opacity: {
+        80: ".8",
+      },
+      zIndex: {
+        2: 2,
+        3: 3,
+      },
+      inset: {
+        "-100": "-100%",
+        "-225-px": "-225px",
+        "-160-px": "-160px",
+        "-150-px": "-150px",
+        "-94-px": "-94px",
+        "-50-px": "-50px",
+        "-29-px": "-29px",
+        "-20-px": "-20px",
+        "25-px": "25px",
+        "40-px": "40px",
+        "95-px": "95px",
+        "145-px": "145px",
+        "195-px": "195px",
+        "210-px": "210px",
+        "260-px": "260px",
+      },
+      height: {
+        "95-px": "95px",
+        "70-px": "70px",
+        "350-px": "350px",
+        "500-px": "500px",
+        "600-px": "600px",
+      },
+      maxHeight: {
+        "860-px": "860px",
+      },
+      maxWidth: {
+        "100-px": "100px",
+        "120-px": "120px",
+        "150-px": "150px",
+        "180-px": "180px",
+        "200-px": "200px",
+        "210-px": "210px",
+        "580-px": "580px",
+      },
+      minWidth: {
+        "140-px": "140px",
+        48: "12rem",
+      },
+      backgroundSize: {
+        full: "100%",
+      },
+    },
+  },
+  variants: [
+    "responsive",
+    "group-hover",
+    "focus-within",
+    "first",
+    "last",
+    "odd",
+    "even",
+    "hover",
+    "focus",
+    "active",
+    "visited",
+    "disabled",
+  ],
+  plugins: [
+    plugin(function ({ addComponents, theme }) {
+      const screens = theme("screens", {});
+      addComponents([
+        {
+          ".container": { width: "100%" },
+        },
+        {
+          [`@media (min-width: ${screens.sm})`]: {
+            ".container": {
+              "max-width": "640px",
+            },
+          },
+        },
+        {
+          [`@media (min-width: ${screens.md})`]: {
+            ".container": {
+              "max-width": "768px",
+            },
+          },
+        },
+        {
+          [`@media (min-width: ${screens.lg})`]: {
+            ".container": {
+              "max-width": "1024px",
+            },
+          },
+        },
+        {
+          [`@media (min-width: ${screens.xl})`]: {
+            ".container": {
+              "max-width": "1280px",
+            },
+          },
+        },
+        {
+          [`@media (min-width: ${screens["2xl"]})`]: {
+            ".container": {
+              "max-width": "1536px",
+            },
+          },
+        },
+      ]);
+    }),
+  ],
+};
